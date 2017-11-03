e50dca441186d9b16191333015b4aecfbf915762
yang <920526047@qq.com>
2017-11-03 09:20:38 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './app/App'
 import router from './router'
-
-Vue.config.productionTip = false
-
+import FastClick from 'fastclick';
+import VueAwesomeSwiper from "vue-awesome-swiper";
+FastClick.attach(document.body);
+Vue.config.productionTip = false;
+Vue.use(VueAwesomeSwiper);
+//vue
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+  	<index-header />
+  	<index-banner />
+  </div>
+</template>
+
+<script>
+	import header from "./header";
+	import banner from "./banner";
+	export default {
+	  name: 'HelloWorld',
+	  data () {
+	    return {
+	      msg: 'Welcome to Your Vue.js App'
+	    }
+	  },
+	  components:{
+	  	"index-header":header,
+	  	"index-banner":banner
+	  }
+	  
+	}
+</script>
+<style scoped>
+</style>
